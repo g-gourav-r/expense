47ed8a2e2ea5bb63412ad655755e067094ad75ee
gourav-ravi <145653975+gourav-ravi@users.noreply.github.com>
2024-03-07 17:03:28 +0530

    New changes in FE

---
+++ temp.py
@@ -0,0 +1,35 @@
+import webbrowser
+
+urls = [
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/calypso-technology-articles-article-affino",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/custodial-integrator",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/marketstream",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/stockgroup-client-portfolio--financial-content",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/stockstream",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/marketstream1",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/lineageview",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/regcloud--highly-secure-fully-managed-cost-optimized",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/regulatory--risk-reporting-calc-capitalcredit-risk-etc",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/data-integrity-and-control-platform--controllerview",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/xp-inc-selects-axiomsl-to-automate-risk-and-regulatory-reporting",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/are-the-complexities-of-ifr-to-investment-firms-as-kryptonite-is-to-superman",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/axiomsl-in-the-lead-to-provide-a-solution-for-ifr-regulatory-requirements",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/vp-bank-selects-axiomsl-to-meet-risk-and-regulatory-reporting-requirements",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/big-data-technology-for-regulatory-reporting-is-getting-lots-of-publicity",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/covid-19-effects-on-nsfr-reporting",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/7-social-distancing-ideas-for-regulatory-reporting-in-the-cloud",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/crr2-reforms-and-beyond-in-the-age-of-covid-19",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/axiomsls-new-sftr-solution-meets-original-esma-reporting-obligation",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/so-many-trade-and-transaction-regimes-so-little-time",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/large-exposure-framework--crr2-alters-fi-reality-globally",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/deposits-customers-accounts-trifecta-winning-the-fdic-part-370-data-race",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/return-on-data-investment-thoughts-from-the-banks-of-data-rivers",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/getting-dynamic-with-data-lineage-perspectives-from-a-regulatory-practitioner",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/ifrs-9-the-data-integration-challenge",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/advisorware-enterprise",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/addressscreener",
+    "https://bobsguidenestg.wpenginepowered.com/directory-articles/anova",
+]
+
+for url in urls:
+    webbrowser.open(url)
